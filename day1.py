6843c7fac2ec39f887881e5abc5d4588f56f01b7
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-12-04 22:16:31 +0100

    day1 part1 finished

--- day1.py
+++ day1.py
@@ -1,1 +1,19 @@
-print("Hello")
+total_diff = 0
+
+with open('input1.txt', 'r') as file:
+    set1 = []
+    set2 = []
+
+    for line in file:
+        left, right = line.strip().split()
+
+        set1.append(int(left))
+        set2.append(int(right))
+
+    set1 = sorted(set1)
+    set2 = sorted(set2)
+
+    total_diff = sum(abs(a - b) for a, b in zip(set1, set2))
+
+print(total_diff)
+
