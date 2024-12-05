3d01b84648a0d1e2c766a6cc13c332cfd38480b2
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-12-05 15:06:50 +0100

    finished day 1 part two
    added other days, still empty

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,5 @@
 total_diff = 0
+total_sim = []
 
 with open('input1.txt', 'r') as file:
     set1 = []
@@ -13,7 +14,17 @@ with open('input1.txt', 'r') as file:
     set1 = sorted(set1)
     set2 = sorted(set2)
 
+# Part One
     total_diff = sum(abs(a - b) for a, b in zip(set1, set2))
 
+# Part Two
+    for num in set1:
+        x = [1 for x in set2 if x == num]
+        total_sim.append(num * sum(x))
+
+# Solution One
 print(total_diff)
 
+
+# Solution Two
+print(sum(total_sim))
